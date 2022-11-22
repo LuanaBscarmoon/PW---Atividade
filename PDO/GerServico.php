e7810bfdaf406290d9e8e332e8f1ad4f13707289
Luana Freire <102596561+LuanaBscarmoon@users.noreply.github.com>
2022-11-22 10:28:45 -0400

    renovado

--- PDO/GerServico.php
+++ PDO/GerServico.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php include '_parts/_linkCSS.php'; ?>
-    <title>Cadastro de Usuario</title>
+    <title>Cadastro de Usuário</title>
 </head>
 
 <body>
